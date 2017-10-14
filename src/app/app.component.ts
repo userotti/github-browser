67876be0d1b370718d89dec5bd4c1735e9b59747
Christian Frauenstein <cfrauenstein@gmail.com>
2017-10-14 14:21:20 +0200

    Added custom bootstrap setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
 }
